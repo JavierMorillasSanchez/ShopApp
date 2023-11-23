0454182f84e819b9da3500af37ede9eb88a0f21e
JavierMS <morillas.sanchez.91@gmail.com>
2023-11-23 13:52:52 +0100

    Renamed main activity, created packages

---
+++ app/src/main/java/com/example/shopapp/features/loginFeature/activity/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopapp.features.loginFeature.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/shopapp/features/shopFeature/activity/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopapp.features.shopFeature.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopapp.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
